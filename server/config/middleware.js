db0bfb110550698c7578b5e0c9b6f4248a1dff6e
Matthew Rourke <mrourke@gmail.com>
2015-05-07 23:19:50 -0400

    Initial user router

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,5 +1,10 @@
 module.exports = function(app, express){
   app.use(express.static(__dirname + '/../../public'));
   app.use(express.static(__dirname + '/../../bower_components'));
+
+  var userRouter = express.Router();
+  require('../users/userRoutes.js')(app, userRouter);
+  
+  app.use('/api/users', userRouter); 
 };
 
