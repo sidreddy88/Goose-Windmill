d9efe5fd5b5ee829e5089fd402104d916a8d12b1
Kenny Tran <kenny8tran@gmail.com>
2015-05-08 15:23:08 -0700

    Added refresh button

--- public/app/app.js
+++ public/app/app.js
@@ -33,5 +33,24 @@ angular.module('hack', [
     return function (text) {
         return $sce.trustAsHtml(text);
     };    
-}]);
+}])
+
+.directive('rotate', function () {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      scope.$watch(attrs.degrees, function (rotateDegrees) {
+        console.log(rotateDegrees);
+        var r = 'rotate(' + rotateDegrees + 'deg)';
+        element.css({
+          '-moz-transform': r,
+          '-webkit-transform': r,
+          '-o-transform': r,
+          '-ms-transform': r
+        });
+      });
+    }
+  }
+});
+;
 
