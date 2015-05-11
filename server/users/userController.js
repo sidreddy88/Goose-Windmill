3cee0e7b6b2bd3cd6119735aa878fa2aad17b7e7
Matthew Rourke <mrourke@gmail.com>
2015-05-11 19:35:21 -0400

    Improve server module documentation

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,9 +1,9 @@
 var User = require('./userModel.js');
 
 module.exports = {
+  //Handles new user account generation
   signup: function(request, response, next) {
 
-    //console.log('REQUEST.BODY: '+JSON.stringify(request.body));
     var username = request.body.username;
     var password = request.body.password;
     var following = request.body.following;
@@ -14,13 +14,15 @@ module.exports = {
       following: following
     };
 
+    //First, determine if the username is available
     User.findOne({username: username}, function(err, user) {
-      //console.log("signup log: ", err, user);
       //User already exists, try again!
       if(user) {
-        //Figure out a way to tell client to redirect to signup
+        //Figure out a way for the client to redirect to the signup page
+        //and inform the user that this username is already in use.
         response.status(400).send('Figure this out Kenny');
       } else {
+        //If it is not in use, create the user in the database
         User.prototype.createUser(params, function(err){
           if(!err){
             response.status(200).send("Signed up");
@@ -32,21 +34,24 @@ module.exports = {
     });    
   },
 
+  //Interact with the database to validate username/password combination
   signin: function(request, response, next) {
     var username = request.body.username;
     var password = request.body.password;
 
     User.prototype.signin(username, password, function(err, results){
       if(!err){
-        console.log('signed in');
+        console.log('Signed in');
         response.status(200).send(results);
       } else {
-        console.log('signin error');
+        console.log('Sign In error');
         response.status(400).send(err);
       }
     })
   },
 
+  //Controller tells the model to update the database when the user adds or 
+  //removes users from their following list
   updateFollowing: function(request, response, next) {
     var username = request.body.username;
     var following = request.body.following;
